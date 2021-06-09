243f78b9f51ff07a622fbc8c85ee53c0c51bb8e8
johnpaolo001 <paolo716@gmail.com>
2021-06-09 22:45:48 +0800

    Adding logs to test case

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -2,26 +2,35 @@ from selenium import webdriver
 import pytest
 from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadCOnfig
+from utilities.customLogger import LogGen
 
 class Test_001_Login:
     baseUrl = ReadCOnfig.getApplicationUrl()
     username = ReadCOnfig.getUseremail()
     password = ReadCOnfig.getPassword()
 
+    logger = LogGen.loggen()
+
 
     def test_homePageTitle(self,setup):
+
+        self.logger.info("********** Test_001_Login **********")
+        self.logger.info("********** Verifying Home Page **********")
         self.driver = setup
         self.driver.get(self.baseUrl)
         act_title = self.driver.title
         if act_title == "Your store. Login":
             assert True
             self.driver.close()
+            self.logger.info("********** Home page title is passed **********")
         else:
             self.driver.save_screenshot("./Screenshots/" + "test_homePageTitle.png")
             self.driver.close()
+            self.logger.error("********** Home page title is failed **********")
             assert False
 
     def test_login(self,setup):
+        self.logger.info("********** Verifying Login Test **********")
         self.driver = setup
         self.driver.get(self.baseUrl)
         self.lp = LoginPage(self.driver)
@@ -31,8 +40,10 @@ class Test_001_Login:
         act_title =self.driver.title
         if act_title == "Dashboard / nopCommerce administration":
             assert True
+            self.logger.info("********** Login Test is passed **********")
             self.driver.close()
         else:
             self.driver.save_screenshot("./Screenshots/" + "test_login.png")
             self.driver.close()
+            self.logger.error("********** Login test is failed **********")
             assert False
