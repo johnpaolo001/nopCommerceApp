15a44237872a5fdadc1fa7f85564b2fa48c487ca
johnpaolo001 <paolo716@gmail.com>
2021-06-12 23:10:02 +0800

    Add Desired Browser

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,20 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome(executable_path="/Users/johnpaolopaulino/Downloads/Web_driver/chromedriver")
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome(executable_path="/Users/johnpaolopaulino/Downloads/Web_driver/chromedriver")
+        print("Launching Chrome browser")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(executable_path="/Users/johnpaolopaulino/Downloads/Web_driver/geckodriver")
+        print("Launching Firefox browser")
+    # else:
+    #     driver = webdriver.Ie()
     return driver
+
+def pytest_addoption(parser): # this will get the value from CLI /hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request): # this will return the Browser value to setup method
+    return request.config.getoption("--browser")
