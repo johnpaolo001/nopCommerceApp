149e696d24efefb69c48047a87a2cc0ad80c754f
johnpaolo001 <paolo716@gmail.com>
2021-06-13 21:16:25 +0800

    Generate pytest HTML Reports

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -19,3 +19,16 @@ def pytest_addoption(parser): # this will get the value from CLI /hooks
 @pytest.fixture()
 def browser(request): # this will return the Browser value to setup method
     return request.config.getoption("--browser")
+
+
+## Pytest HTML Report ##
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'nop Commerce'
+    config._metadata['MOdule Name'] = 'Customers'
+    config._metadata['Tester'] = 'Pavan'
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
